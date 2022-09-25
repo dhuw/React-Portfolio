e5b208916c2c843adc0b46993ecf43551e31c0fa
dhuw <hazlewood.dyl@gmail.com>
2022-09-24 17:47:45 -0700

    uncomment

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-// import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Home from "./home";
 import Login from '../Login';
 
